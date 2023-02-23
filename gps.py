0a87183f92a3ea6a27e08c18e1fd19923c1b8176
Romain PIERRE <romain.pierre@enseirb-matmeca.fr>
2023-02-23 16:39:57 +0100

    Kilometers and path in the tk window directly

--- gps.py
+++ gps.py
@@ -65,6 +65,8 @@ for i in  range(30):
 # Set Itinerary
 start = 0
 destination = 0
+result = "Select a start and a destination"
+kilometers = " "
 
 # ---------- Program ----------
 
@@ -77,6 +79,12 @@ def closest_unexplored_city():
         if (explored[i] == 0) and (distances_to_start[i] < distances_to_start[closest]):
             closest = i
     return closest
+
+def number_to_name(n):
+    for name, member in Cities.__members__.items():
+            if (member.value == n):
+                return name
+    return "No city match"
 
 def itinerary_name(T):
     travel = []
@@ -91,11 +99,17 @@ def value_city(name_city):
     for name, member in Cities.__members__.items():
         if name_city == name:
             return member.value
+    
+def reset_gps():
+    global explored, distances_to_start
+    for i in range(number_cities):
+        explored[i] = 0
+        distances_to_start[i] = inf
 
 # Dijkstra algorithm
 def find_travel():
+    global kilometers
     distances_to_start[start] = 0
-
     while (explored[destination] == 0):
         current_city = closest_unexplored_city()
         explored[current_city] = 1
@@ -104,13 +118,12 @@ def find_travel():
                 if distances_to_start[current_city] + G[current_city][i] < distances_to_start[i]:
                     distances_to_start[i] = distances_to_start[current_city] + G[current_city][i]
                     previous_city[i] = current_city
-
     back = destination
     itinerary = [back]
     while(back != start):
         itinerary.append(previous_city[back])
         back = previous_city[back]
-
+    kilometers = distances_to_start[destination]
     return itinerary_name(itinerary)
 
 # Tkinter
@@ -118,35 +131,56 @@ root = tk.Tk()
 root.geometry('1000x500')
 
 def start_action(event):
-    global start
+    global start, result
     choosen_number = start_combo.get()
     start = value_city(choosen_number)
-    print(find_travel())
+    result = find_travel()
+    label_result.config(text = result)
+    label_kilo.config(text = "Kilometers : " + str(kilometers))
+    reset_gps()
 
 label_start = tk.Label(root, text = "Start ?")
-label_start.pack()
 
 start_combo = ttk.Combobox(root, values=list_cities)
  
 start_combo.current(0)
 
-start_combo.pack()
 start_combo.bind("<<ComboboxSelected>>", start_action)
 
 def destination_action(event):
-    global destination
+    global destination, result
     choosen_number = destination_combo.get()
     destination = value_city(choosen_number)
-    print(find_travel())
+    result = find_travel()
+    label_result.config(text = result)
+    label_kilo.config(text = "Kilometers : " + str(kilometers))
+    reset_gps()
 
-label_destination = tk.Label(root, text = "destination ?")
-label_destination.pack()
+label_destination = tk.Label(root, text = "Destination ?")
 
 destination_combo = ttk.Combobox(root, values=list_cities)
  
 destination_combo.current(0)
 
-destination_combo.pack()
 destination_combo.bind("<<ComboboxSelected>>", destination_action)
+
+#label for space
+label_space = tk.Label(root, text = " ")
+label_space2 = tk.Label(root, text = " ")
+label_space3 = tk.Label(root, text = " ")
+
+label_result = tk.Label(root, text = result)
+
+label_kilo = tk.Label(root, text = " ")
+
+label_start.pack()
+start_combo.pack()
+label_destination.pack()
+destination_combo.pack()
+label_space.pack()
+label_space2.pack()
+label_result.pack()
+label_space3.pack()
+label_kilo.pack()
 
 root.mainloop()
